24e5bceda4822a609c93d4ebcf998f1a72e477ce
Edward Dorta <edwarddorta@edwards-air.myfiosgateway.com>
2022-12-11 15:57:47 -0500

    added try catch on get

--- pages/api/urls.js
+++ pages/api/urls.js
@@ -2,7 +2,15 @@
 import { connectToDatabase } from '../../utils/mongodb';
 
 export default async function handler(req, res) {
-  const { db } = await connectToDatabase();
-  const data = await db.collection('urls').find({}).toArray();
-  res.status(200).json({ urls: data })
+  try {
+    const { db } = await connectToDatabase();
+    const data = await db.collection('urls').find({}).toArray();
+    res.status(200).json({ urls: data })
+    
+  } catch (error) {
+    res.status(500);
+    res.json({
+      error:error.message
+    })
+  }
 }
